97fa84c858bbabb96231def17b919f2841740cff
Hannes Devillé <hannes.deville@gmail.com>
2014-04-18 22:46:20 +0200

    changed web look

--- html/js/main.js
+++ html/js/main.js
@@ -10,41 +10,34 @@ var mShowGrid = true;
 var mRedrawRequested = true;
 var mGridWidth = 30;
 var mGridHeight = 20;
-var mPixelSize = 32;
+var mPixelSize = 20;
 
 var mCurrentGridData;
 var colorPalette = ["rgb(255,0,0)","rgb(0,255,0)","rgb(0,0,255)"];
 
 /* Function definitions */
-var fnUpdate = function() {};
-var fnDraw = function() {};
-
-var fnDrawGrid = function()
+var fnUpdate = function()
 {
-	if(mShowGrid && mRedrawRequested)
-	{
-		mRedrawRequested = false;
-
-		for (var x=0;x<mGridWidth;++x)
-		{
-			for (var y=0;y<mGridHeight;++y)
-			{
-		    	// Find the starting index in the one-dimensional image data
-				var i = y*mGridWidth + x;
-				/*
-				var r = imgData[i];
-				*/
-				
-				var colorIndex = parseInt(mCurrentGridData.substring(i,i+1));
-				
-				mCtx.fillStyle = colorPalette[colorIndex];
-				mCtx.fillRect( x*mPixelSize, y*mPixelSize, mPixelSize, mPixelSize);
-			}
-		}
-	}
+    
 };
 
-function initApp(){
+function drawGrid()
+{
+	for (var x=0;x<mGridWidth;++x)
+    {
+        for (var y=0;y<mGridHeight;++y)
+        {
+            // Find the starting index in the one-dimensional image data
+            var i = y*mGridWidth + x;
+            var colorIndex = parseInt(mCurrentGridData.substring(i,i+1));
+            mCtx.fillStyle = colorPalette[colorIndex];
+            mCtx.fillRect( x*mPixelSize, y*mPixelSize, mPixelSize, mPixelSize);
+        }
+    }
+}
+
+function initApp()
+{
 	/* Size The Canvas */
 	$("#canvas").attr("width", mGridWidth*mPixelSize+"px");
 	$("#canvas").attr("height", mGridHeight*mPixelSize+"px");
@@ -54,13 +47,7 @@ function initApp(){
 	/* Init time manager */
 	mTime = new Time();
 	
-	$.get('pixeldata.txt', function(data)
-	{
-		mCurrentGridData = data;
-		
-		/* start drawing the grid */
-		fnDraw = fnDrawGrid;
-	}, 'text');
+	getLatestData();
 
 	/* Start the main loop */
 	tick();
@@ -75,7 +62,14 @@ function tick()
 	drawScene();
 }
 
-function drawScene()
+function getLatestData()
 {
-	fnDraw();
+    $.get('pixeldata.txt', function(data)
+    {
+        if(mCurrentGridData != data)
+        {
+          mCurrentGridData = data;
+          drawGrid();
+        }
+    }, 'text');
 }
